366d8bbad8ff2513e996fe1e48cade8c481c1663
Bruno <brunoleandroperez@hotmail.com>
2023-08-14 20:23:33 -0300

    trabajos en primer entrega 14/08/2023

--- entregable_4-PrimeraEntrega/src/ProductManager.js
+++ entregable_4-PrimeraEntrega/src/ProductManager.js
@@ -1,64 +1,95 @@
-import { promises as fs } from 'fs'
+import { promises as fs } from "fs";
 //const fs = require("fs");
 
 export class ProductManager {
   constructor(path) {
     this.path = path;
     this.products = [];
-    this.id = 11;
   }
 
-  async addProducts({ title, description, price, thumbnail, code, stock, id }) {
-    id = this.id;
-    const verificarCode = this.products.some((product) => {
+  async generateIndex(listProducts) {
+    try {
+      if (listProducts.length === 0) return 1;
+      return listProducts[listProducts.length - 1].id + 1;
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: armyManager.js:17 ~ ArmyManager ~ generateIndex ~ error",
+        error
+      );
+    }
+  }
+
+  async addProducts({
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock,
+    id,
+    status,
+    category,
+  }) {
+    const listProducts = JSON.parse(await fs.readFile(this.path, "utf-8"));
+    const newId = await this.generateIndex(listProducts);
+    id = newId;
+    status = true;
+    const verificarCode = listProducts.some((product) => {
       return product.code === code;
     });
     if (verificarCode) {
-      console.log("El valor de code ya se encuentra asignado a otro producto");
+      return {
+        message: "el valor del CODE ya se encuentra asignado a otro producto",
+      };
     } else if (
       title != "" &&
       description != "" &&
       price != "" &&
-      thumbnail != "" &&
       stock != "" &&
       title != undefined &&
       description != undefined &&
       price != undefined &&
-      thumbnail != undefined &&
       stock != undefined &&
       code != "" &&
-      code != undefined
+      code != undefined &&
+      category != "" &&
+      category != undefined
     ) {
       try {
-        console.log("producto cargado correctamente");
-        this.products.push({
+        // console.log("producto cargado correctamente");
+        listProducts.push({
           title,
           description,
           price,
           thumbnail,
           code,
           stock,
+          status,
+          category,
           id,
         });
 
-        this.id = this.id + 1;
-        await fs.writeFile(this.path, JSON.stringify(this.products));
+        await fs.writeFile(this.path, JSON.stringify(listProducts));
       } catch (error) {
-      console.log("🚀 ~ file: ProductManager.js:46 ~ ProductManager ~ addProducts ~ error:", error)
+        console.log(
+          "🚀 ~ file: ProductManager.js:46 ~ ProductManager ~ addProducts ~ error:",
+          error
+        );
       }
     } else {
-      console.log("Todos los parametros son requeridos");
+      return { message: "todos los parametros son requeridos" };
     }
   }
 
   async getProducts() {
     try {
       const data = JSON.parse(await fs.readFile(this.path, "utf-8"));
-      //console.log("🚀 ~ file: ProductManager.js:60 ~ ProductManager ~ getProducts ~ data:", data)
       return data;
     } catch (error) {
-      console.log("🚀 ~ file: ProductManager.js:59 ~ ProductManager ~ getProducts ~ error:", error)
-      ;
+      console.log(
+        "🚀 ~ file: ProductManager.js:59 ~ ProductManager ~ getProducts ~ error:",
+        error
+      );
     }
   }
 
@@ -73,34 +104,37 @@ export class ProductManager {
   };
 
   updateProduct = async (id, data) => {
+    let listProducts = await this.getProducts()
+    //let idProduct = listProducts.find((e) => e.id === id);
     try {
-      let productoAActualizar = await this.getProductById(id);
+      let productToUpdate = await this.getProductById(id);
       let productoIndex = this.products.findIndex((e) => e.id === id);
-      this.products[productoIndex] = {
-        ...productoAActualizar,
+      listProducts[productoIndex] = {
+        ...productToUpdate,
         ...data,
         id: id,
       };
-      await fs.writeFile(this.path, JSON.stringify(this.products));
-      console.log("Producto editado correctamente");
+      await fs.writeFile(this.path, JSON.stringify(listProducts));
+      return {message: "Producto actualizado correctamente"};
     } catch (error) {
-    console.log("🚀 ~ file: ProductManager.js:84 ~ ProductManager ~ updateProduct= ~ error:", error)
+    console.log("🚀 ~ file: ProductManager.js:120 ~ ProductManager ~ updateProduct= ~ error:", error)
     }
   };
 
   deleteProduct = async (id) => {
     try {
-      this.products = JSON.parse(
-        await fs.promises.readFile(this.path, "utf-8")
-      );
-      this.products = this.products.filter((prod) => prod.id !== id);
-      await fs.writeFile(this.path, JSON.stringify(this.products));
+      let listProducts = await this.getProducts();
+      listProducts = listProducts.filter((prod) => prod.id !== id);
+      await fs.writeFile(this.path, JSON.stringify(listProducts));
+      return { message: "Producto eliminado" };
     } catch (error) {
-      console.log("🚀 ~ file: ProductManager.js:99 ~ ProductManager ~ deleteProduct ~ error:", error);
+      console.log(
+        "🚀 ~ file: ProductManager.js:112 ~ ProductManager ~ deleteProduct ~ error:",
+        error
+      );
     }
   };
 }
-
 
 // export * from ProductManager;
 // //module.exports = ProductManager;
