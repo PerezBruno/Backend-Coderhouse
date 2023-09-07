f133057f8d66577071f34f94afa137cba3272d63
Bruno <brunoleandroperez@hotmail.com>
2023-09-07 03:51:22 -0300

    rutas de api completas - falta todo views

--- entregable_6-Practica/src/index.js
+++ entregable_6-Practica/src/index.js
@@ -2,6 +2,8 @@ import App from "./app.js";
 
 import cartsRoutes from "./routes/carts.routes.js";
 
+import chatRoutes from "./routes/chat.routes.js";
+
 import  usersRoutes from "./routes/users.routes.js"
 
 import productsRoutes from "./routes/products.routes.js";
@@ -11,8 +13,16 @@ const app = new App([
   new cartsRoutes(),
   new usersRoutes(),
   new productsRoutes(),
+  new chatRoutes(),
   //new ViewsRoutes(),
 ]);
 
 
 app.listen();
+
+
+//TODO: falta:
+// poner fecha y hora a los mensajes del chat
+//cargar las vistas de handlebars
+//ver como implementar socket.io en POO
+//
